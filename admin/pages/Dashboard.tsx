fec302be3cca0aad262608b789905d08fbaa9cd2
Victory Nwani <vickywane@gmail.com>
2023-11-05 16:45:09 +0000

    feat: remove contember files & generate boilerplate SanityCMS project

--- admin/pages/Dashboard.tsx
+++
@@ -1,8 +0,0 @@
-import { GenericPage, TitleBar } from '@contember/admin'
-
-export const DashboardPage = (
-	<GenericPage pageName="dashboard">
-		<TitleBar> Portfolio CMS </TitleBar>
-		Victory Portolio Admin Page!
-	</GenericPage>
-)
